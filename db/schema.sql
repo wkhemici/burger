7e89b99f8b41d68833e1a1ca0400492d8dccf09e
Wassim Khemici <wkhemici@berkeley.edu>
2019-04-04 11:40:35 -0700

    initial scaffolding

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db; 
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(45) NULL,
+	devoured BOOLEAN,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
+
+SET SQL_SAFE_UPDATES = 0;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Cheese Burger", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Chicken Burger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Guacamole Burger", false)
+
