afb1fd97204b63d81d6b688c15ba9bff2ea4b3ea
Wassim Khemici <wkhemici@berkeley.edu>
2019-04-04 14:23:57 -0700

    preliminary scaffolding after structuring directory

--- server.js
+++ server.js
@@ -1,11 +1,50 @@
-//set connection to orm.js  aka require 
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
 
-//In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+// Create an instance of the express app.
+var app = express();
 
-// selectAll()
-// insertOne()
-// updateOne()
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3000;
 
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+// Data
+var burgers = [
+  {
+    burgers: "Cheese Burger",
+    devoured: false
+  }, {
+    burgers: "Chicken Burger",
+    devoured: false
+  }, {
+    burgers: "Guacamole Burger",
+    devoured: false
+  }
+];
 
-// Export the ORM object in module.exports.
+// Routes
+app.get("/home", function(req, res) {
+  res.render("index", burgers[0]);
+});
+
+app.get("/", function(req, res) {
+  res.render("index", burgers[1]);
+});
+
+app.get("/burgers", function(req, res) {
+  res.render("all-burgers", {
+    burgers: burgers,
+    devoured: devoured
+  });
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
