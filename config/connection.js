6d664ed2de0a045d1fc0d43f4b850312d682aa1d
Wassim Khemici <wkhemici@berkeley.edu>
2019-04-28 11:33:08 -0700

    heroku bug

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Algeria1!",
-  database: "burgers_db"
-});
+var connection; 
+
+if (process.nextTick.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Algeria1!",
+    database: "burgers_db"
+  })
+}
 
 // Make connection.
 connection.connect(function(err) {
@@ -23,6 +29,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
